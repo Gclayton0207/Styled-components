e97b4fb2bc629f043f93f95f0931ba80a5ff769e
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-05-20 09:08:06 -0300

    adicioando GlobalStyle

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import {GlobalStyle} from "./Components/GlobalStyle";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Cabecalho />
       <Container />
+
     </>
   );
 }
