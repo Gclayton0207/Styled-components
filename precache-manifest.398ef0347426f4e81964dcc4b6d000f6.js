88aca8a8427ebf1584d956add2ce9a0161ccf025
Gclayton0207 <Gclayton0207@users.noreply.github.com>
2022-05-24 11:28:33 +0000

    deploy: 826accd34fe53defeff1b6d49f7b71ab47214609

---
+++ precache-manifest.398ef0347426f4e81964dcc4b6d000f6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f503a4d0c4fc9a80837c10770559c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e9901dfa9ff4aa87e59",
+    "url": "/static/js/2.0fb40ae9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0fb40ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0628c441c97b309c426",
+    "url": "/static/js/main.74ba47b9.chunk.js"
+  },
+  {
+    "revision": "927d5e5b2fc5e8a904b3",
+    "url": "/static/js/runtime-main.e6b65449.js"
+  },
+  {
+    "revision": "44dbaa78fb2ed240792c9e7a424b0ea0",
+    "url": "/static/media/alimentacao.44dbaa78.svg"
+  },
+  {
+    "revision": "f3a2bff49642ca68b3841fde422295cb",
+    "url": "/static/media/bank_logo.f3a2bff4.svg"
+  },
+  {
+    "revision": "b97c300bed11302a8687c16e07aa89e5",
+    "url": "/static/media/dinheiro.b97c300b.svg"
+  },
+  {
+    "revision": "acd41ceb3366a71c61b7ed3d5df83682",
+    "url": "/static/media/olho.acd41ceb.svg"
+  },
+  {
+    "revision": "3945d28ae3434a318d43369b325ea61b",
+    "url": "/static/media/outros.3945d28a.svg"
+  },
+  {
+    "revision": "134d853a95b2b30676da2c63cda2eeaf",
+    "url": "/static/media/privado.134d853a.svg"
+  },
+  {
+    "revision": "605ef9ca7e70a1bb568ffcec443d05f6",
+    "url": "/static/media/saude.605ef9ca.svg"
+  },
+  {
+    "revision": "0981f5bfd5f197c52e678c3c0616cd81",
+    "url": "/static/media/themeOff.0981f5bf.svg"
+  },
+  {
+    "revision": "75ea28146734a5ea6593dab45d272a90",
+    "url": "/static/media/themeOn.75ea2814.svg"
+  },
+  {
+    "revision": "15a4ed7310630a72e21a8054897c3a51",
+    "url": "/static/media/transporte.15a4ed73.svg"
+  },
+  {
+    "revision": "59035811601fe0a607827eb6408e14ef",
+    "url": "/static/media/utilidades.59035811.svg"
+  }
+]);
